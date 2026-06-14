a3495baf5fcca0c95dbaa82e2b0aed733fcc7123
Kevin Santamaria <kevsantamaria01@gmail.com>
2026-06-14 19:56:32 -0400

    feat(config): add to app's module the config module and auth guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,17 @@ import { PostsModule } from './posts/posts.module';
 import { DatabaseModule } from './database.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { AuthGuard } from './auth/auth.guard';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, DatabaseModule, UsersModule, PostsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    DatabaseModule,
+    UsersModule,
+    PostsModule,
+  ],
+  providers: [{ provide: 'APP_GUARD', useClass: AuthGuard }],
 })
 export class AppModule {}
